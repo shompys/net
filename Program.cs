14a4a3ba7f637562bac1e382bab3de2392656c3d
shompys <jonathangomezit@gmail.com>
2022-10-05 01:10:33 -0300

    saved

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 
-var school = new School();
+var school = new CoreSchool.Entities.School("university Jonas", "av. siempreviva 123", 1900, "argentina", "Buenos Aires");
 
 school.Timbrar();
+Console.WriteLine(school);
 
--- classes/School.cs
+++ classes/School.cs
@@ -1,8 +1,32 @@
-class School {
-    string? name;
-    string? address;
+namespace CoreSchool.Entities {
 
-    public void Timbrar() {
-        Console.WriteLine($"Riiing");
+    class School {
+        // string name;
+        
+        // public string Name {
+        //     get { return name; }
+        //     set { name = value.ToUpper(); }
+        // }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int YearCreated { get; set; }
+
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        // public School(string name, string address, int yearCreated, string country, string city) {
+        //     this.name = name;
+        //     this.address = address;
+        //     this.YearCreated = yearCreated;
+        //     this.Country = country;
+        //     this.City = city;
+        // }
+        public School(string name, string address, int yearCreated, string country, string city
+        ) => (Name, Address, YearCreated, Country, City) = (name, address, yearCreated, country, city);
+
+        public void Timbrar() {
+            Console.WriteLine($"Riiing");
+        }
     }
 }
